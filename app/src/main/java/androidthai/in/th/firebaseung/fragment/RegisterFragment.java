e26044eb501294ba62bebff35282f1258f55f76b
MasterUNG <phrombutr@gmail.com>
2017-11-25 15:02:01 +0700

    Back Controller Success

--- app/src/main/java/androidthai/in/th/firebaseung/fragment/RegisterFragment.java
+++ app/src/main/java/androidthai/in/th/firebaseung/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package androidthai.in.th.firebaseung.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidthai.in.th.firebaseung.MainActivity;
 import androidthai.in.th.firebaseung.R;
 
 /**
@@ -14,6 +16,49 @@ import androidthai.in.th.firebaseung.R;
  */
 
 public class RegisterFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }   // Main Method
+
+    private void createToolbar() {
+
+//        Config Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title and Sub
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setSubtitle(getString(R.string.sub_register));
+
+//        Back Icon
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getActivity().getSupportFragmentManager()
+                        .popBackStack();
+
+            }
+        });
+
+
+
+    }
+
 
     @Nullable
     @Override
