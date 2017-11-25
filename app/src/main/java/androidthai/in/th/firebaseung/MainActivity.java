df58bc977f92f147fe9f303faaef530f13277cd1
MasterUNG <phrombutr@gmail.com>
2017-11-25 11:58:17 +0700

    Design UI Main Finish

--- app/src/main/java/androidthai/in/th/firebaseung/MainActivity.java
+++ app/src/main/java/androidthai/in/th/firebaseung/MainActivity.java
@@ -3,11 +3,25 @@ package androidthai.in.th.firebaseung;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidthai.in.th.firebaseung.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+
+//            Add Fragment to Activity
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
+
+
+    }   // Main Method
+
+}   // Main Class
---
+++ app/src/main/java/androidthai/in/th/firebaseung/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package androidthai.in.th.firebaseung.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidthai.in.th.firebaseung.R;
+
+/**
+ * Created by masterung on 25/11/2017 AD.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}   // Main Class
