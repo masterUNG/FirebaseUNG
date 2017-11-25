3cbea2ec115171dfa6d825d28d7d4fed7331b8c9
MasterUNG <phrombutr@gmail.com>
2017-11-25 14:28:06 +0700

    Replace Fragment Success

--- app/src/main/java/androidthai/in/th/firebaseung/fragment/MainFragment.java
+++ app/src/main/java/androidthai/in/th/firebaseung/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidthai.in.th.firebaseung.R;
 
@@ -14,6 +15,30 @@ import androidthai.in.th.firebaseung.R;
  */
 
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        registerController();
+
+    }   // Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null).commit();
+
+            }   // onClick
+        });
+    }
 
     @Nullable
     @Override
