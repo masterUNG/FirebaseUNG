0691d900cdb1d2aea645843fca6fae8cb5525f0f
MasterUNG <phrombutr@gmail.com>
2017-11-25 15:20:23 +0700

    Create Menu

--- app/src/main/java/androidthai/in/th/firebaseung/fragment/RegisterFragment.java
+++ app/src/main/java/androidthai/in/th/firebaseung/fragment/RegisterFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -24,8 +26,21 @@ public class RegisterFragment extends Fragment {
 //        Create Toolbar
         createToolbar();
 
+//        Create Menu Icon
+        setHasOptionsMenu(true);
+
 
     }   // Main Method
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        inflater.inflate(R.menu.menu_save, menu);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+
 
     private void createToolbar() {
 
